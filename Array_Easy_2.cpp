39bb1252399f7ec3c4ab04ab82017683d8903d09
Nanzvx <anandacbca@gmail.com>
2025-06-05 18:59:05 +0530

    Union of two sorted arrays are added

--- Array_Easy_2.cpp
+++ Array_Easy_2.cpp
@@ -51,6 +51,31 @@ vector<int> moveZeros(int n,vecorr<int> a){
     return a;
 }
 
+//Union of two Sorted Arrays
+vector<int> unionarray(vector<int> a,vector<int> b){
+    int n1 = a.size();
+    int n2 = b.size();
+    int i = 0;
+    int j = 0;
+    vector<int> unionarr;
+    while(i<n1 && j<n2){
+        if(a[i]<= b[j]){
+            if(unionarr.size()==0||
+        unionarr.back()!=a[i]){
+            unionarr.push_back(a[i]);
+        }
+        i++;
+        }
+        else{
+              if(unionarr.size()==0||
+        unionarr.back()!=b[j]){
+            unionarr.push_back(b[j]);
+        }
+        j++;
+        }
+    }
+    return unionarr;
+}
 //Main Function
 int main(){
     int n;
