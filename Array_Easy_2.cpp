105fcaad566f6b93e12d9043f622aa1ada1d68db
Nanzvx <anandacbca@gmail.com>
2025-06-05 18:05:49 +0530

    rotate an array by D places has been added

--- Array_Easy_2.cpp
+++ Array_Easy_2.cpp
@@ -10,7 +10,27 @@ vector<int> rorateArray(vector<int> &arr){
     arr[n-1]=temp;
     return arr;
 }
-
+//Left Rotate array by D places
+  void reverse(vector<int>& arr,int i,int j)
+    {
+        while(i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+  
+    void rotateArr(vector<int>& arr, int d) {
+       
+        int n = arr.size();
+        d = d % n;
+        reverse(arr, 0 , d - 1);
+        reverse(arr, d , arr.size() - 1);
+        reverse(arr, 0, arr.size() - 1);
+        
+    }
 
 
 
