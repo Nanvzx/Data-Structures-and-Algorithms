c33d7b80c1af03a04f5632dd2b5826d6b13dedc3
Nanzvx <anandacbca@gmail.com>
2025-06-07 22:03:54 +0530

    Maximum consecutive ones in an array is added

--- Array_Easy_3.cpp
+++ Array_Easy_3.cpp
@@ -11,7 +11,21 @@ int missingNumber(vector<int> &a,int n){
     }
     return xor1 ^ xor2;
 }
-
+// Finding the max consecutive ones
+int findmaxones(vector<int> &arr){
+    int maxi =0;
+    int cnt = 0;
+    for(int i = 0;i<arr.size();i++){
+        if(arr[i]==1){
+            cnt ++;
+            maxi = max(maxi,cnt); 
+        }
+        else{
+            cnt = 0;
+        }
+    }
+    return maxi;
+}
 
 
 
